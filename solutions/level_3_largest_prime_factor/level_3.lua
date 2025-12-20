324eaa1bfc264cb28628f10bb26cee74c92ca068
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2025-12-20 03:42:56 -0500

    Update level_3.lua

--- solutions/level_3_largest_prime_factor/level_3.lua
+++ solutions/level_3_largest_prime_factor/level_3.lua
@@ -1,7 +1,7 @@
 -- filename: level_3.lua
 -- Project Euler Level 3 solution in Lua
 -- https://projecteuler.net/problem=3
--- [GITHUB LINK]
+-- https://github.com/K0K0SHA/Project_Euler/edit/main/solutions/level_3_largest_prime_factor/level_3.lua
 -- challenge: The prime factors of 13195 are 5, 7, 13, and 29. What is the largest prime factor of the number 600851475143?
 -- hint: this challenge can be solved a lot of ways, but one way to create prime numbers is the Sieve of Eratosthenes
 -- This code implements the Sieve of Eratosthenes, which is a way to find primes by factoring out numbers divisible by 2...sqrt(n) where n = target
@@ -23,5 +23,6 @@ if n > 1 then
     largest_factor = n
 end
 
+print("largest prime factor of 600851475143 =\n")
 print(largest_factor)
 
